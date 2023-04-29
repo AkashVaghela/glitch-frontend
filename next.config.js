999d8251a680e60cf19bd559257b0e0260feef62
Akash Vaghela <akashvaghela.dev@gmail.com>
2023-04-29 19:08:44 +0530

    updated config file

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: [
-    "forgot-password.styles.ts",
-    "reset-password.styles.ts",
-    "sign-up.styles.ts",
-    "slug.styles.ts",
-    "category.styles.ts",
-    "bookmarks.styles.ts",
-    "profile.styles.ts",
-  ],
 };
 
 module.exports = nextConfig;
